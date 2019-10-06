cf1c6998ce9c1c7f0abc37c4d4dbf0426e7a0734
Kristen Sundquist <kssundquist@gmail.com>
2019-10-06 07:54:56 -0700

    Add App root where components will be displayed

---
+++ src/App/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PhoneNumberInput from "../PhoneNumberInput";
+
+export const App: React.FC = () => (
+    <div>
+        <h2>Phone Number Input</h2>
+        <PhoneNumberInput />
+    </div>
+);
